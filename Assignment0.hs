23c174b71207ad526775612d4e6824d41c164f6e
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2022-02-09 02:32:17 +0100

    deleted work function

--- Assignment0.hs
+++ Assignment0.hs
@@ -3,5 +3,4 @@ main :: IO ()
 -- 2
 -- main = putStrLn "Hello, world!"
 -- 3 Interaction with the outside world
-main = interact work
-  where work = unlines . map reverse . lines
+main = interact (unlines . map reverse . lines)
