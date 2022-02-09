26ef1ea7f1be38a2e2dca58a09ac6f4ed684a291
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2022-02-09 02:43:41 +0100

    intercalate /

--- Assignment0.hs
+++ Assignment0.hs
@@ -1,6 +1,10 @@
 module Main where
+import Data.List (intercalate)
 main :: IO ()
 -- 2
 -- main = putStrLn "Hello, world!"
 -- 3 Interaction with the outside world
-main = interact (unlines . map reverse . lines)
+-- main = interact (unlines . map reverse . lines)
+-- 4 The exercise
+main = interact work
+  where work = intercalate " / " . map reverse . lines
