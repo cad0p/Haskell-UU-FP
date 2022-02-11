1772a2f0edf39704b2e0e37a6ae9ba4276f3a541
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2022-02-12 00:54:37 +0100

    ok kinda works without control of number of chars alignment works

--- Assignment1.hs
+++ Assignment1.hs
@@ -1,7 +1,8 @@
 -- Assignment 1: Lists
 module Main where
-import           Data.List                      ( intercalate )
 
+import           Data.Char                      ( isDigit )
+import           Data.List                      ( intercalate )
 
 main :: IO ()
 
@@ -16,11 +17,33 @@ parseTable = map words . lines
 
 type Width = Int
 
-replicateL :: Int -> String
+replicateL :: Width -> String
 replicateL x = replicate x '-'
 
+-- | The 'printLine' function prints a line
 printLine :: [Width] -> String
 printLine = wrapPlus . intercalate "+" . map replicateL
   where wrapPlus x = concat ["+", x, "+"]
+
+{-|
+  given a desired width for a field and the contents of a field, 
+  returns a formatted field by adding additional whitespace. 
+  If the field only consists of numerical digits, 
+  the field should be right-aligned, 
+  otherwise it should be left-aligned.
+-}
+printField
+  :: Int      -- ^ desired width for the field
+  -> String   -- ^ contents of the field
+  -> String   -- ^ formatted field
+printField w c = formatField (isFieldNumerical c) w c
+ where
+  isFieldNumerical = all isDigit
+  
+formatField :: Bool -> Width -> String -> Field
+-- left aligned
+formatField False w c = concat (c : replicate w " ")
+formatField True  w c = concat (replicate w " " ++ [c])
+
 
 main = putStrLn (printLine [5, 6, 6, 6])
